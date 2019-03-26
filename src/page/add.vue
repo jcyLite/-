24f19151c27de3ee8bedbe5bc37f03f58a65502b
chenjian <jsdtcj@126.com>
2019-03-26 14:55:02 +0800

    是

---
+++ src/page/add.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="page-add">
+		<div class="header">
+			add
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				a:[]
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
